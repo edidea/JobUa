0df83f26f9ca00b3df8570d95d8a460fa444a5bd
edidea <mariakr.work@gmail.com>
2025-07-04 13:31:51 +0300

    Job Ua

---
+++ src/main/java/com/ua/jobua/JobUaApplication.java
@@ -0,0 +1,13 @@
+package com.ua.jobua;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobUaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JobUaApplication.class, args);
+    }
+
+}
